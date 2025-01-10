fefba54776991e97e811878d97ffae785b5d14bc
victorandujar <victorandujarmurcia@gmail.com>
2025-01-10 14:07:38 +0100

    Add link to logo to go to home + underline navbar section active

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Navbar from "../Navbar/Navbar";
+import Link from "next/link";
 
 const Header = (): React.ReactElement => {
   return (
     <header className="w-full flex justify-center items-center px-4">
-      <Image src={"/mango-logo.png"} alt="logo" width={150} height={100} />
+      <Link href={"/"}>
+        <Image src={"/mango-logo.png"} alt="logo" width={150} height={100} />
+      </Link>
       <section className="absolute right-20">
         <Navbar />
       </section>
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import { navbarSections } from "./utils/navbarSections";
+import { usePathname } from "next/navigation";
 
 const Navbar = (): React.ReactElement => {
+  const pathname = usePathname();
+
   return (
     <nav className="flex items-center gap-10">
       {navbarSections.map((section) => (
         <Link
           href={section.link}
           key={section.id}
-          className="text-sm font-bold tracking-wide uppercase"
+          className={`text-sm font-bold tracking-wide uppercase ${pathname === section.link && "underline"}`}
         >
           {section.name}
         </Link>
