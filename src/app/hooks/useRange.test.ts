f3dda71b4c2333c8fcd74c335d361126c212cd3a
victorandujar <victorandujarmurcia@gmail.com>
2025-01-10 13:11:54 +0100

    Add testing for useRange hook functions

--- src/app/hooks/useRange.test.ts
+++ src/app/hooks/useRange.test.ts
@@ -1,11 +1,11 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
 import useRange from "../hooks/useRange";
 import {
   HandlePositions,
   MaxPositionParams,
   MinPositionParams,
 } from "../interfaces/Range";
-import { RefObject } from "react";
+import { RefObject, act } from "react";
 
 describe("Given a useRange hook", () => {
   describe("When handleMinPosition is called with its params", () => {
@@ -24,20 +24,17 @@ describe("Given a useRange hook", () => {
         max: 100,
       };
 
-      const {
-        result: {
-          current: { handleMinPosition },
-        },
-      } = renderHook(() => useRange());
+      const { result } = renderHook(() => useRange());
 
-      handleMinPosition(params);
-
-      await act(() => {
-        expect(setMinPosition).toHaveBeenCalledWith(50);
-        expect(setMin).toHaveBeenCalledWith(20);
+      await act(async () => {
+        result.current.handleMinPosition(params);
       });
+
+      expect(setMinPosition).toHaveBeenCalledWith(50);
+      expect(setMin).toHaveBeenCalledWith(20);
     });
   });
+
   describe("When handleMaxPosition is called with its params", () => {
     test("Then it should update max position correctly", async () => {
       const setMaxPosition = jest.fn();
@@ -54,18 +51,14 @@ describe("Given a useRange hook", () => {
         rangeWidth: 100,
       };
 
-      const {
-        result: {
-          current: { handleMaxPosition },
-        },
-      } = renderHook(() => useRange());
+      const { result } = renderHook(() => useRange());
 
-      handleMaxPosition(params);
-
-      await act(() => {
-        expect(setMaxPosition).toHaveBeenCalledWith(80);
-        expect(setMax).toHaveBeenCalledWith(40);
+      await act(async () => {
+        result.current.handleMaxPosition(params);
       });
+
+      expect(setMaxPosition).toHaveBeenCalledWith(80);
+      expect(setMax).toHaveBeenCalledWith(40);
     });
   });
 
@@ -98,15 +91,11 @@ describe("Given a useRange hook", () => {
         minPosition: 30,
       };
 
-      const {
-        result: {
-          current: { handleDrag },
-        },
-      } = renderHook(() => useRange());
+      const { result } = renderHook(() => useRange());
 
       let position;
       await act(async () => {
-        position = handleDrag(
+        position = result.current.handleDrag(
           "min",
           { clientX: 60 } as MouseEvent,
           rangeRef as unknown as RefObject<HTMLDivElement>,
@@ -115,9 +104,383 @@ describe("Given a useRange hook", () => {
       });
 
       expect(position).toBe(50);
-
       expect(setMinPosition).toHaveBeenCalledWith(50);
       expect(setMin).toHaveBeenCalledWith(20);
+    });
+  });
+
+  describe("When handleKeyDownMin is called", () => {
+    test("Then it should update max value correctly for fixed type when ArrowLeft is pressed", async () => {
+      const setMin = jest.fn();
+      const setMinPosition = jest.fn();
+
+      const event = { key: "ArrowLeft" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "fixed";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMin(
+          event,
+          setMin,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMinPosition,
+        );
+      });
+
+      expect(setMin).toHaveBeenCalled();
+    });
+
+    test("Then it should update max value correctly for fixed type when ArrowRight is pressed", async () => {
+      const setMin = jest.fn();
+      const setMinPosition = jest.fn();
+
+      const event = { key: "ArrowRight" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "fixed";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMin(
+          event,
+          setMin,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMinPosition,
+        );
+      });
+
+      expect(setMin).toHaveBeenCalled();
+      expect(setMinPosition).toHaveBeenCalledWith(40);
+    });
+
+    test("Then it should update max value correctly for normal type when ArrowLeft is pressed", async () => {
+      const setMax = jest.fn();
+      const setMaxPosition = jest.fn();
+
+      const event = { key: "ArrowLeft" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "normal";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMin(
+          event,
+          setMax,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMaxPosition,
+        );
+      });
+
+      expect(setMax).toHaveBeenCalled();
+    });
+
+    test("Then it should update max value correctly for normal type when ArrowLeft is pressed", async () => {
+      const setMax = jest.fn();
+      const setMaxPosition = jest.fn();
+
+      const event = { key: "ArrowRight" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "normal";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMin(
+          event,
+          setMax,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMaxPosition,
+        );
+      });
+
+      expect(setMax).toHaveBeenCalled();
+    });
+  });
+
+  describe("When handleKeyDownMax is called", () => {
+    test("Then it should update max value correctly for fixed type when ArrowLeft is pressed", async () => {
+      const setMax = jest.fn();
+      const setMaxPosition = jest.fn();
+
+      const event = { key: "ArrowLeft" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "fixed";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMax(
+          event,
+          setMax,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMaxPosition,
+        );
+      });
+
+      const expectedPosition = (30 / 50) * rangeWidth;
+
+      expect(setMax).toHaveBeenCalledWith(30);
+      expect(setMaxPosition).toHaveBeenCalledWith(expectedPosition);
+    });
+
+    test("Then it should update max value correctly for fixed type when ArrowRight is pressed", async () => {
+      const setMax = jest.fn();
+      const setMaxPosition = jest.fn();
+
+      const event = { key: "ArrowRight" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "fixed";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMax(
+          event,
+          setMax,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMaxPosition,
+        );
+      });
+
+      const expectedPosition = (50 / 50) * rangeWidth;
+
+      expect(setMax).toHaveBeenCalledWith(50);
+      expect(setMaxPosition).toHaveBeenCalledWith(expectedPosition);
+    });
+
+    test("Then it should update max value correctly for normal type when ArrowLeft is pressed", async () => {
+      const setMax = jest.fn();
+      const setMaxPosition = jest.fn();
+
+      const event = { key: "ArrowLeft" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "normal";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMax(
+          event,
+          setMax,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMaxPosition,
+        );
+      });
+
+      expect(setMax).toHaveBeenCalled();
+    });
+
+    test("Then it should update max value correctly for normal type when ArrowLeft is pressed", async () => {
+      const setMax = jest.fn();
+      const setMaxPosition = jest.fn();
+
+      const event = { key: "ArrowRight" } as React.KeyboardEvent;
+
+      const rangeValues = [0, 10, 20, 30, 40, 50];
+      const type = "normal";
+      const min = 10;
+      const max = 40;
+      const rangeWidth = 100;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleKeyDownMax(
+          event,
+          setMax,
+          min,
+          max,
+          rangeValues,
+          type,
+          rangeWidth,
+          setMaxPosition,
+        );
+      });
+
+      expect(setMax).toHaveBeenCalled();
+    });
+  });
+
+  describe("When handleMinChange is called", () => {
+    test("Then it should handle min value change and feedback", async () => {
+      const setMin = jest.fn().mockResolvedValue(5);
+      const setErrorFeedbackMax = jest.fn().mockResolvedValue(true);
+      const setErrorFeedbackMin = jest.fn();
+
+      const event = {
+        target: { value: "50" },
+      } as React.ChangeEvent<HTMLInputElement>;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleMinChange(
+          event,
+          setMin,
+          setErrorFeedbackMax,
+          setErrorFeedbackMin,
+        );
+      });
+
+      expect(setMin).toHaveBeenCalledWith(50);
+    });
+
+    test("Then it should handle errors when values are lower than 0 or greater than 100", async () => {
+      const setMin = jest.fn().mockResolvedValue(5);
+      const setErrorFeedbackMax = jest.fn();
+      const setErrorFeedbackMin = jest.fn();
+
+      const eventMin = {
+        target: { value: "-1" },
+      } as React.ChangeEvent<HTMLInputElement>;
+
+      const eventMax = {
+        target: { value: "200" },
+      } as React.ChangeEvent<HTMLInputElement>;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleMinChange(
+          eventMin,
+          setMin,
+          setErrorFeedbackMax,
+          setErrorFeedbackMin,
+        );
+      });
+
+      await act(async () => {
+        result.current.handleMinChange(
+          eventMax,
+          setMin,
+          setErrorFeedbackMax,
+          setErrorFeedbackMin,
+        );
+      });
+
+      expect(setErrorFeedbackMin).toHaveBeenCalled();
+      expect(setErrorFeedbackMax).toHaveBeenCalled();
+    });
+  });
+
+  describe("When handleMaxChange is called", () => {
+    test("Then it should handle max value change and feedback", async () => {
+      const setMax = jest.fn();
+      const setErrorFeedbackMax = jest.fn();
+      const setErrorFeedbackMin = jest.fn();
+
+      const event = {
+        target: { value: "80" },
+      } as React.ChangeEvent<HTMLInputElement>;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleMaxChange(
+          event,
+          setMax,
+          setErrorFeedbackMax,
+          setErrorFeedbackMin,
+        );
+      });
+
+      expect(setMax).toHaveBeenCalledWith(80);
+    });
+
+    test("Then it should handle errors when values are lower than 0 or greater than 100", async () => {
+      const setMin = jest.fn().mockResolvedValue(5);
+      const setErrorFeedbackMax = jest.fn();
+      const setErrorFeedbackMin = jest.fn();
+
+      const eventMin = {
+        target: { value: "-1" },
+      } as React.ChangeEvent<HTMLInputElement>;
+
+      const eventMax = {
+        target: { value: "200" },
+      } as React.ChangeEvent<HTMLInputElement>;
+
+      const { result } = renderHook(() => useRange());
+
+      await act(async () => {
+        result.current.handleMaxChange(
+          eventMin,
+          setMin,
+          setErrorFeedbackMax,
+          setErrorFeedbackMin,
+        );
+      });
+
+      await act(async () => {
+        result.current.handleMaxChange(
+          eventMax,
+          setMin,
+          setErrorFeedbackMax,
+          setErrorFeedbackMin,
+        );
+      });
+
+      expect(setErrorFeedbackMin).toHaveBeenCalled();
+      expect(setErrorFeedbackMax).toHaveBeenCalled();
     });
   });
 });
